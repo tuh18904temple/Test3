d5e85f55d8b3e0242b759a4bff52fb9857dc67cb
SL <tuh18904@temple.edu>
2022-11-29 14:21:54 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/tuh18904/test3/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.temple.tuh18904.test3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
